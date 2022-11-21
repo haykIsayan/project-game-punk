55d29b8fcef7e5faf0a558c23f4b74a45658f38c
Hayk <haykisayan@gmail.com>
2022-11-21 08:19:17 -0800

    Implemented paging carousel in now playing section.

--- app/src/main/java/com/example/project_game_punk/features/discover/playing/NowPlayingSection.kt
+++ app/src/main/java/com/example/project_game_punk/features/discover/playing/NowPlayingSection.kt
@@ -3,12 +3,11 @@ package com.example.project_game_punk.features.discover.playing
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import com.example.project_game_punk.features.common.composables.GameCarouselItem
-import com.example.project_game_punk.features.common.composables.ItemCarousel
+import com.example.project_game_punk.features.common.composables.GamePagerCarouselItem
 import com.example.project_game_punk.features.common.composables.LoadableStateWrapper
 import com.example.project_game_punk.features.common.composables.SectionTitle
+import com.example.project_game_punk.features.common.composables.carousels.ItemPagerCarousel
 import com.example.project_game_punk.features.discover.DiscoverGameCarouselLoading
-import com.example.project_game_punk.features.discover.components.DiscoverGameCarouselSection
 import com.example.project_game_punk.features.discover.components.DiscoverGameFailState
 
 @Composable
@@ -21,13 +20,8 @@ fun NowPlayingSection(nowPlayingViewModel: NowPlayingViewModel) {
             failState = { errorMessage -> DiscoverGameFailState(errorMessage) { nowPlayingViewModel.loadState() } },
             loadingState = { DiscoverGameCarouselLoading() },
         ) { games ->
-            ItemCarousel(items = games) { game ->
-                GameCarouselItem(
-                    game = game,
-                    sheetController = null
-                ) { game, gameProgress ->
-
-                }
+            ItemPagerCarousel(items = games) { game ->
+                GamePagerCarouselItem(game = game)
             }
         }
     }
